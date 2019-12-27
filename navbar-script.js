b35b1a2b324cd77e6282969965cbe22210dae3c8
Rafay Khawar <rafaykhawar09@gmail.com>
2019-12-27 15:02:06 -0800

    uploading files

---
+++ navbar-script.js
@@ -0,0 +1,52 @@
+
+var navbar = $("#nav");
+var hamburger = $(".hamburger");
+var navGroup = $(".nav-group");
+var navLinks = $(".nav-link");
+
+console.log(navbar);
+console.log(hamburger);
+
+hamburger.on("click", function(){
+     
+     if(!(hamburger.hasClass("change")))
+          hamburger.addClass("change");
+     else
+          hamburger.removeClass("change");
+
+     if(navGroup.hasClass("hidden")){
+          navGroup.slideDown("slow");
+          navGroup.removeClass("hidden");
+     }
+     else{
+          navGroup.slideUp("slow");
+          navGroup.addClass("hidden");
+     }
+})
+
+navLinks.on("click", function(){
+     
+     if(hamburger.hasClass("change"))
+          hamburger.removeClass("change");
+
+     if(!(navGroup.hasClass("hidden"))){
+          navGroup.slideUp("slow");
+          navGroup.addClass("hidden");
+     }
+});
+
+$(window).scroll(function(){
+     
+     if($(window).width() > 1000){
+
+          navbar.css("background-color", "rgba(0,0,0,0)")
+
+          if($(window).scrollTop() > 100){
+               navbar.css("background-color", "black");
+          }
+     }
+     else{
+          navbar.css("background-color", "black")
+     }
+});
+
